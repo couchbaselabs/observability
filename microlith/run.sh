dd457d34af222668cf1e7bb84cb40064b4b97cf3
Patrick Stephens <patrick.stephens@couchbase.com>
2021-05-21 13:51:44 +0100

    Switched to simple stdout with prefix for source

--- microlith/run.sh
+++ microlith/run.sh
@@ -2,11 +2,12 @@
 set -e
 for i in /entrypoints/*; do
     if [[ -x "$i" ]]; then
-        LOGFILE=${LOGS_DIR:-/logs}/${i##/entrypoints/}.log
-        echo "Running: $i > $LOGFILE"
-        tini -- "$i" "$@" &> "${LOGFILE}" &
+        EXE_NAME=${i##/entrypoints/}
+        # See https://github.com/hilbix/speedtests for log name pre-pending info
+        echo "[ENTRYPOINT] Running: $i"
+        tini -- "$i" "$@" 2>&1 | awk '{ print "['"${EXE_NAME}"']" $0 }' &
     else
-        echo "Skipping non-executable: $i"
+        echo "[ENTRYPOINT] Skipping non-executable: $i"
     fi
 done
 
