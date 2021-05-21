4bd0e80d20f5129ea3c4ee5c337111899af8bd18
Patrick Stephens <patrick.stephens@couchbase.com>
2021-05-21 11:50:07 +0100

    Add launch scripts

---
+++ microlith/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+for i in /entrypoints/*; do
+    if [[ -x "$i" ]]; then
+        echo "Running: $i"
+        if [[ "$i" == "/entrypoints/loki" ]]; then
+            tini -- "$i" "-config.file=/etc/loki/local-config.yaml" &
+        elif [[ "$i" == "/entrypoints/prometheus" ]]; then
+            tini -- "$i" "--config.file=/etc/prometheus/prometheus.yml" \
+             "--storage.tsdb.path=/prometheus" \
+             "--web.console.libraries=/usr/share/prometheus/console_libraries" \
+             "--web.console.templates=/usr/share/prometheus/consoles" &
+        else
+            tini -- "$i" "$@" &
+        fi
+    else
+        echo "Skipping non-executable: $i"
+    fi
+done
+
+wait -n
