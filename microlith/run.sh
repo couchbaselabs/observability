4fb889a713deaf93f7f8585d2b491e974e995005
Patrick Stephens <patrick.stephens@couchbase.com>
2021-05-21 14:05:40 +0100

    Add support for disabling certain things by env var

--- microlith/run.sh
+++ microlith/run.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -e
 for i in /entrypoints/*; do
-    if [[ -x "$i" ]]; then
-        EXE_NAME=${i##/entrypoints/}
+    EXE_NAME=${i##/entrypoints/}
+    UPPERCASE=${EXE_NAME^^}
+    DISABLE_VAR=DISABLE_${UPPERCASE%%.*}
+    # Set DISABLE_XXX to skip running
+    if [[ -v "${DISABLE_VAR}" ]]; then
+        echo "[ENTRYPOINT] Disabled as ${DISABLE_VAR} set (value ignored): $i"
+    elif [[ -x "$i" ]]; then
         # See https://github.com/hilbix/speedtests for log name pre-pending info
         echo "[ENTRYPOINT] Running: $i"
         tini -- "$i" "$@" 2>&1 | awk '{ print "['"${EXE_NAME}"']" $0 }' &
