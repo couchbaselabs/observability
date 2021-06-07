931ccd1f3d1d054bc15cb2949eb614297e116aaa
Patrick Stephens <patrick.stephens@couchbase.com>
2021-06-07 16:07:08 +0100

    Refactored to properly support env vars (cannot use minus)

--- microlith/run.sh
+++ microlith/run.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
-
-# https://stackoverflow.com/a/5586663
-trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
 
 # Expose all nested config variables to make it simple to seeCLUSTER_MONITOR_USER=${CLUSTER_MONITOR_USER:-admin}
 export CLUSTER_MONITOR_PWD=${CLUSTER_MONITOR_PWD:-password}
@@ -15,20 +12,26 @@ export HOST_ROOTFS=${NODE_EXPORTER_HOST_ROOTFS:-/node-exporter/host/rootfs}
 export HOST_SYS=${NODE_EXPORTER_HOST_SYS:-/node-exporter/host/sys}
 export CUSTOM_COLLECTOR=${NODE_EXPORTER_CUSTOM_COLLECTOR:-/node-exporter/custom}
 
-for i in /entrypoints/*; do
-    EXE_NAME=${i##/entrypoints/}
-    UPPERCASE=${EXE_NAME^^}
-    DISABLE_VAR=DISABLE_${UPPERCASE%%.*}
-    # Set DISABLE_XXX to skip running
-    if [[ -v "${DISABLE_VAR}" ]]; then
-        echo "[ENTRYPOINT] Disabled as ${DISABLE_VAR} set (value ignored): $i"
-    elif [[ -x "$i" ]]; then
-        # See https://github.com/hilbix/speedtests for log name pre-pending info
-        echo "[ENTRYPOINT] Running: $i"
-        tini -- "$i" "$@" 2>&1 | awk '{ print "['"${EXE_NAME}"']" $0 }' &
-    else
-        echo "[ENTRYPOINT] Skipping non-executable: $i"
-    fi
-done
+# Support passing in custom command to run, e.g. bash
+if [[ $# -gt 0 ]]; then
+    echo "[ENTRYPOINT] Running custom: $*"
+    exec "$@"
+else
+    for i in /entrypoints/*; do
+        EXE_NAME=${i##/entrypoints/}
+        UPPERCASE=${EXE_NAME^^}
+        DISABLE_VAR=DISABLE_${UPPERCASE%%.*}
+        # Set DISABLE_XXX to skip running
+        if [[ -v "${DISABLE_VAR}" ]]; then
+            echo "[ENTRYPOINT] Disabled as ${DISABLE_VAR} set (value ignored): $i"
+        elif [[ -x "$i" ]]; then
+            # See https://github.com/hilbix/speedtests for log name pre-pending info
+            echo "[ENTRYPOINT] Enabled as ${DISABLE_VAR} not set: $i"
+            "$i" "$@" 2>&1 | awk '{ print "['"${EXE_NAME}"']" $0 }' &
+        else
+            echo "[ENTRYPOINT] Skipping non-executable: $i"
+        fi
+    done
 
-wait -n
+    wait -n
+fi
