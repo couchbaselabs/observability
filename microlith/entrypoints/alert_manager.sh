e9f20eabaa692801f1e52dd69c9bdadfaf77e409
Patrick Stephens <patrick.stephens@couchbase.com>
2021-06-15 15:39:57 +0100

    Added alert manager via proxy Updated to provide example of disabling default rule

--- microlith/entrypoints/alert_manager.sh
+++ microlith/entrypoints/alert_manager.sh
@@ -2,5 +2,6 @@
 set -ex
 ALERTMANAGER_CONFIG_FILE=${ALERTMANAGER_CONFIG_FILE:-/etc/alertmanager/config.yml}
 ALERTMANAGER_STORAGE_PATH=${ALERTMANAGER_STORAGE_PATH:-/alertmanager}
+ALERTMANAGER_URL_SUBPATH=${ALERTMANAGER_URL_SUBPATH-/alertmanager/}
 
-/bin/alertmanager --config.file="${ALERTMANAGER_CONFIG_FILE}" --storage.path="${ALERTMANAGER_STORAGE_PATH}"
+/bin/alertmanager --config.file="${ALERTMANAGER_CONFIG_FILE}" --storage.path="${ALERTMANAGER_STORAGE_PATH}"  --web.route-prefix="${ALERTMANAGER_URL_SUBPATH}"
