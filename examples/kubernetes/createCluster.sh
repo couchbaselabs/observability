b3720ae6295d691683f289904377d705751b907e
Patrick Stephens <patrick.stephens@couchbase.com>
2021-06-30 11:26:01 +0100

    Bug fix

--- examples/kubernetes/createCluster.sh
+++ examples/kubernetes/createCluster.sh
@@ -16,14 +16,9 @@ set -eux
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-CONFIG_DIR=$(mktemp -d)
 CLUSTER_NAME=${CLUSTER_NAME:-microlith-test}
-CLUSTER_CONFIG="${CONFIG_DIR}/multinode-cluster-conf.yaml"
-
 SKIP_CLUSTER_CREATION=${SKIP_CLUSTER_CREATION:-no}
-
 SERVER_IMAGE=${SERVER_IMAGE:-couchbase/server:6.6.2}
-SERVER_COUNT=${SERVER_COUNT:-3}
 
 set +x
 
@@ -32,11 +27,10 @@ if [[ "${SKIP_CLUSTER_CREATION}" != "yes" ]]; then
   kind delete cluster --name="${CLUSTER_NAME}"
 
   # Simple script to deal with running up a test cluster for KIND for developing logging updates for.
+  CLUSTER_CONFIG=$(mktemp)
   cat << EOF > "${CLUSTER_CONFIG}"
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
-featureGates:
- EphemeralContainers: true
 nodes:
 - role: control-plane
   kubeadmConfigPatches:
@@ -52,17 +46,13 @@ nodes:
   - containerPort: 443
     hostPort: 443
     protocol: TCP
-EOF
-
-    for i in $(seq "$SERVER_COUNT"); do
-      echo "Adding worker $i"
-      cat << EOF >> "${CLUSTER_CONFIG}"
+- role: worker
+- role: worker
 - role: worker
 EOF
-    done
 
   kind create cluster --name="${CLUSTER_NAME}" --config="${CLUSTER_CONFIG}"
-  rm -rf "${CONFIG_DIR}"
+  rm -f "${CLUSTER_CONFIG}"
 
   kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
   kubectl wait --namespace ingress-nginx \
@@ -82,14 +72,14 @@ kubectl apply -f "${SCRIPT_DIR}/microlith.yaml"
 # Add Couchbase via helm chart
 helm repo add couchbase https://couchbase-partners.github.io/helm-charts/
 helm repo update
-helm upgrade --install couchbase couchbase/couchbase-operator --set cluster.image="${SERVER_IMAGE}",cluster.monitoring.prometheus.enabled=true
+helm upgrade --install couchbase couchbase/couchbase-operator --set cluster.image="${SERVER_IMAGE}" --values="${SCRIPT_DIR}/custom-values.yaml"
 
 # Wait for deployment to complete
 echo "Waiting for CB to start up..."
-until [[ $(kubectl get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq $SERVER_COUNT ]]; do
+until [[ $(kubectl get pods --field-selector=status.phase=Running --selector='app=couchbase' --no-headers 2>/dev/null |wc -l) -eq 3 ]]; do
     echo -n '.'
     sleep 2
 done
-echo "CB started"
+echo "CB running"
 
-# TODO: run local container as well that talks to the cluster
+echo "To monitor go to http://localhost/"
