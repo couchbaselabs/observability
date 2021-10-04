1a8cf7b82ab7f11e46480dcda40df0a544bf1fa1
Patrick Stephens <patrick.stephens@couchbase.com>
2021-10-04 14:44:09 +0100

    Refactor based on review comments

--- testing/kubernetes/run.sh
+++ testing/kubernetes/run.sh
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -eu
+
+set -xueo pipefail
 
 DOCKER_USER=${DOCKER_USER:-couchbase}
 DOCKER_TAG=${DOCKER_TAG:-v1}
