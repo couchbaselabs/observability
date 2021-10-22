5c4e986e053145466f7094d8af522a7673c8aec6
Patrick Stephens <patrick.stephens@couchbase.com>
2021-10-22 14:21:24 +0100

    Update licensing info with binary licensing details plus Tern output

--- tools/tern-report.sh
+++ tools/tern-report.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Simple script to analyse the microlith container with Tern: https://github.com/tern-tools/tern
-set -eu
+set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DOCKER_USER=${DOCKER_USER:-couchbase}
@@ -24,17 +24,21 @@ CMOS_IMAGE=${CMOS_IMAGE:-$DOCKER_USER/observability-stack:$DOCKER_TAG}
 # Ensure we have the container built
 "$SCRIPT_DIR/build-oss-container.sh"
 
+# Cleanup
 if [[ -d "$SCRIPT_DIR/tern" ]]; then
-    pushd "$SCRIPT_DIR/tern"
-        git pull
-    popd
-else
-    mkdir -p "$SCRIPT_DIR/tern"
-    git clone https://github.com/tern-tools/tern.git "$SCRIPT_DIR/tern"
+    rm -rf "$SCRIPT_DIR/tern"
 fi
 
+mkdir -p "$SCRIPT_DIR/tern"
+git clone --depth 1 https://github.com/tern-tools/tern.git "$SCRIPT_DIR/tern"
+
 # Build the Tern container
-docker build -f docker/Dockerfile -t ternd "$SCRIPT_DIR/tern"
+docker build -f "$SCRIPT_DIR/tern/docker/Dockerfile" -t ternd "$SCRIPT_DIR/tern"
+
 # Now run it against CMOS, once to give human-readable output in the terminal and once for an HTML report we can detain.
 docker run --privileged --device /dev/fuse -v /var/run/docker.sock:/var/run/docker.sock --rm ternd report --docker-image "$CMOS_IMAGE"
-docker run --privileged --device /dev/fuse -v /var/run/docker.sock:/var/run/docker.sock --rm ternd report --report-format html --docker-image "$CMOS_IMAGE" > "$SCRIPT_DIR/tern-output.html"
+docker run --privileged --device /dev/fuse -v /var/run/docker.sock:/var/run/docker.sock --rm ternd report --report-format html --docker-image "$CMOS_IMAGE" > "$SCRIPT_DIR/../microlith/html/tern-licensing-report.html"
+
+# Cleanup
+docker rmi ternd
+rm -rf "$SCRIPT_DIR/tern"
