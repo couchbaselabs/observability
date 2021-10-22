829869f3c82804a20a6233ddcda1f36633560afb
redm2 <87416390+redm2@users.noreply.github.com>
2021-10-22 12:44:19 +0100

    Add unique alphabetical names to cluster provisioning code
    Also change variables to local to avoid conflict

--- examples/native/helpers/driver.sh
+++ examples/native/helpers/driver.sh
@@ -16,7 +16,7 @@
 
 source "$SCRIPT_DIR"/helpers/vagrants_up.sh
 
-# Remove all vagrants: vagrant global-status | awk '$1 ~ /[0-9,a-f]{6}/{system("vagrant destroy -f "$1)}'
+# Remove all vagrants: vagrant global-status --prune | awk '$1 ~ /[0-9,a-f]{6}/{system("vagrant destroy -f "$1)}'
 # Remove all boxes: vagrant box list | cut -f 1 -d ' ' | xargs -L 1 vagrant box remove -f --all
 
 # Parameters:
@@ -65,25 +65,31 @@ function configure_servers() {
     DATA_ALLOC=$(awk -v n="$VAGRANT_RAM" 'BEGIN {printf "%.0f\n", (n*0.7)}')
     INDEX_ALLOC=$(awk -v n="$VAGRANT_RAM" 'BEGIN {printf "%.0f\n", (n*0.7)}')
 
-    all_nodes=($(vagrant global-status | grep "$CB_VERSION/$VAGRANT_OS" | awk '$1 ~ /[0-9,a-f]{6}/{system("echo "$1)}'))
-    nodes_left=$VAGRANT_NODES
+    local NAMES=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+
+    local all_nodes=($(vagrant global-status | grep "$CB_VERSION/$VAGRANT_OS" | awk '$1 ~ /[0-9,a-f]{6}/{system("echo "$1)}'))
+    local nodes_left=$VAGRANT_NODES
 
     for ((i=0; i<CLUSTER_NUMBER; i++))
     do
 
-        to_provision=$(( nodes_left / (CLUSTER_NUMBER - i) )) # This is always integer division, Bash does not support decimals
-        start=$(( VAGRANT_NODES - nodes_left ))
-        first_uid=${all_nodes[$start]}
+        local to_provision=$(( nodes_left / (CLUSTER_NUMBER - i) )) # This is always integer division, Bash does not support decimals
+        local start=$(( VAGRANT_NODES - nodes_left ))
+        local first_uid=${all_nodes[$start]}
         
         for uid in "${all_nodes[@]:start:to_provision}" # Slice of length $to_provision, beginning at $start
             do
                 
                 if [[ $uid == "$first_uid" ]]; then
                     # Create and configure new cluster
+                    local ip
                     ip=$(vagrant ssh "$uid" -c "hostname -I | cut -d' ' -f2" -- -q | tail -n1)
                     ip="${ip%%[[:cntrl:]]}" # Remove \r from IP string
 
-                    vagrant ssh "$uid" -c "/opt/couchbase/bin/couchbase-cli cluster-init -c $ip  --cluster-username=$SERVER_USER \
+                    local x=$(( 26 - (26 - (VAGRANT_NODES-nodes_left)) ))
+                    local clust_name="Cluster ${NAMES[$x]}"
+
+                    vagrant ssh "$uid" -c "/opt/couchbase/bin/couchbase-cli cluster-init -c $ip  --cluster-name=\"$clust_name\" --cluster-username=$SERVER_USER \
                         --cluster-password=$SERVER_PASS --cluster-ramsize=$DATA_ALLOC --cluster-index-ramsize=$INDEX_ALLOC --services=data"
 
                     # Load sample buckets and register cluster with CBMM
@@ -100,12 +106,13 @@ function configure_servers() {
 
                 else
                     # Add server to existing cluster
+                    local to_add_ip
                     to_add_ip=$(vagrant ssh "$uid" -c "hostname -I | cut -d' ' -f2" -- -q | tail -n1)
                     to_add_ip="${to_add_ip%%[[:cntrl:]]}"
 
-                    vagrant ssh "$uid" -c "/opt/couchbase/bin/couchbase-cli node-init --cluster $ip --username $SERVER_USER --password $SERVER_PASS \
-                        && /opt/couchbase/bin/couchbase-cli server-add -c $ip --username $SERVER_USER --password $SERVER_PASS --server-add $to_add_ip \
-                        --server-add-username $SERVER_USER --server-add-password $SERVER_PASS --services index,query"
+                    vagrant ssh "$uid" -c "/opt/couchbase/bin/couchbase-cli node-init --cluster $ip --username $SERVER_USER --password $SERVER_PASS"
+                    vagrant ssh "$uid" -c "/opt/couchbase/bin/couchbase-cli server-add -c $ip --username $SERVER_USER --password $SERVER_PASS \
+                        --server-add $to_add_ip --server-add-username $SERVER_USER --server-add-password $SERVER_PASS --services index,query"
                 fi
             done
 
@@ -115,7 +122,7 @@ function configure_servers() {
             --no-progress-bar --no-wait"
         fi
 
-        nodes_left=$((nodes_left - to_provision))
+        local nodes_left=$((nodes_left - to_provision))
     done
 
 }
