fba91f2ab8228ea6b4aed9eeb1c1285b47399c4d
redm2 <87416390+redm2@users.noreply.github.com>
2021-11-04 13:36:49 +0000

    Update examples/containers/multi/helpers/driver.sh
    Co-authored-by: Marks Polakovs <marks.polakovs@couchbase.com>

--- examples/containers/multi/helpers/driver.sh
+++ examples/containers/multi/helpers/driver.sh
@@ -50,8 +50,17 @@ function start_new_nodes() {
         done
 
         ready=true
-        for b in "${nodes_ready[@]}"; do if ! $b; then ready=false; fi; done
-        if $ready; then break; else echo "..." && sleep 5; fi
+        for b in "${nodes_ready[@]}"; do
+            if ! $b; then
+                ready=false
+            fi
+        done
+        if $ready; then
+            break
+        else
+            echo "..."
+            sleep 5
+        fi
         # Bash does not support boolean operators on true/false and the evaluation of 0 or 1 as true/false changes 
         # depending on the context and would be much harder to understand
     done
