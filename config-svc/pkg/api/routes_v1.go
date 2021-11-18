877d1f9ab88752e321cee380e62002ac030faeb7
Marks Polakovs <marks.polakovs@couchbase.com>
2021-11-18 10:57:54 +0000

    Add API endpoint and HTML page to collect information

--- config-svc/pkg/api/routes_v1.go
+++ config-svc/pkg/api/routes_v1.go
@@ -19,6 +19,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/couchbase/tools-common/cbvalue"
 	"github.com/couchbaselabs/observability/config-svc/pkg/couchbase"
@@ -30,6 +31,7 @@ import (
 )
 
 const defaultPrometheusConfigPath = "/etc/prometheus/prometheus.yml"
+const collectInfoPath = "/collect-information.sh"
 
 func (s *Server) PostClustersAdd(ctx echo.Context) error {
 	var data v1.PostClustersAddJSONRequestBody
@@ -104,6 +106,20 @@ func (s *Server) PostClustersAdd(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, map[string]interface{}{
 		"ok": true,
 	})
+}
+
+func (s *Server) PostCollectInformation(ctx echo.Context) error {
+	cmd := exec.Command(collectInfoPath)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	return ctx.Stream(http.StatusOK, "text/plain", stdout)
 }
 
 func createScrapeConfigForCluster(cluster *couchbase.PoolsDefault, useTLS bool, username,
