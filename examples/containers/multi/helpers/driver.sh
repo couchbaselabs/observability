956aa2f60e9fcb931f029dd18af344394ae89511
redm2 <87416390+redm2@users.noreply.github.com>
2021-11-25 14:53:46 +0000

    Update driver.sh

--- examples/containers/multi/helpers/driver.sh
+++ examples/containers/multi/helpers/driver.sh
@@ -78,8 +78,8 @@ function start_new_nodes() {
 # - The command fails $retry_count times in a row and the program exits
 function _docker_exec_with_retry() {
 
-    local retry_count=4
-    local retry_time=(5 10 15 30 60)
+    local retry_count=10
+    local retry_time=(5 10 10 10 10 10 10 10 10 10)
 
     local container=$1
     local command=$2
