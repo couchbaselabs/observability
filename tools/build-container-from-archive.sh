1734fe2fba711f90c48c73353f550c94148b797d
Marks Polakovs <marks.polakovs@couchbase.com>
2022-03-29 11:06:26 +0100

    CBD-4609: Refactor build process (#256)

---
+++ tools/build-container-from-archive.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 Couchbase, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file  except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the  License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ $# -eq 0 ]; then
+    echo "No image file supplied."
+    echo "Usage: build-container-from-archive.sh path/to/image.tgz"
+    exit 1
+fi
+
+file=$1
+
+if [ $# -eq 2 ]; then
+    tag=$2
+else
+    file_basename=$(basename "$file")
+    tag=${file_basename%%.tgz}
+    tag=${tag%%.tar.gz}
+    tag=$(echo "$tag" | sed -E 's/^couchbase-(.+)-image_(.+)$/couchbase\/\1:\2/')
+fi
+
+
+echo "Test-building Docker image $tag..."
+tmpdir=$(mktemp -d)
+tar -C "$tmpdir" -zxvf "$file"
+docker build -t "$tag" --build-arg PROD_VERSION=0.2.0 --build-arg PROD_BUILD=278 "$tmpdir"
