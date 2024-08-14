50e9a2de22232867f0110b7dbdded891dfdbf9b4
Jose Cortes Diaz <jcortesd@gmail.com>
2024-08-14 17:09:54 +0200

    adding fluent-bit module

--- installgo.sh
+++
@@ -1,8 +0,0 @@
-wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
-source ~/.profile
-go version
-echo "export GOPATH=$HOME/go" >> ~/.profile
-echo "export PATH=$PATH:$GOPATH/bin" >> ~/.profile
-source ~/.profile
